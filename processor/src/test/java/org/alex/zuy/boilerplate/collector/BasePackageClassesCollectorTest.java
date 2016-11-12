42421c8b1cfafe5622a1e9a2c0071a73ed6eb40b
Zuy Alexey <zuy.alexey@gmail.com>
2016-11-12 21:02:44 +0300

    Replace mocking BasePackageClassesCollector test with JCA based one

--- processor/src/test/java/org/alex/zuy/boilerplate/collector/BasePackageClassesCollectorTest.java
+++ processor/src/test/java/org/alex/zuy/boilerplate/collector/BasePackageClassesCollectorTest.java
@@ -1,83 +1,45 @@
 package org.alex.zuy.boilerplate.collector;
 
-import static org.alex.zuy.boilerplate.collector.ElementTestUtils.makeElementOfKind;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
 
-import org.alex.zuy.boilerplate.ProcessorContextImpl;
+import org.alex.zuy.boilerplate.collector.support.AnnotationProcessorBase;
+import org.alex.zuy.boilerplate.collector.support.TypeElementsSetMatcher;
 import org.alex.zuy.boilerplate.services.ProcessorContext;
-import org.alex.zuy.boilerplate.utils.CollectionUtils;
-import org.junit.Before;
-import org.junit.Rule;
+import org.alex.zuy.boilerplate.support.TestBuildSetupBuilder;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 
 public class BasePackageClassesCollectorTest {
 
-    private static final String PACKAGE_NAME = "com.example.main";
-
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
-
-    @Mock
-    private RoundEnvironment roundEnvironment;
-
-    @Mock
-    private Elements elementUtils;
-
-    private BasePackageClassesCollector collector;
-
-    private Set<TypeElement> collectedClasses;
-
-    @Before
-    public void setUp() {
-        ProcessorContext processorContext = new ProcessorContextImpl(elementUtils, null, null,
-            null, null);
-        collector = new BasePackageClassesCollector(processorContext);
-    }
-
     @Test
-    public void testCollectorReturnsOnlyClasses() throws Exception {
-        givenPackageWithDifferentTypesOfElements();
-        whenPackageElementsProcessed();
-        thenCollectorShouldContainOnlyClass();
+    public void testCollector() throws Exception {
+        Boolean result = TestBuildSetupBuilder.newInstance()
+            .addTestSpecificSources(this.getClass())
+            .addAnnotationProcessor(new AnnotationProcessorImpl())
+            .createCompileTask(null).call();
+        assertTrue(result);
     }
 
-    private void givenPackageWithDifferentTypesOfElements() {
-        List<Element> packageElements = Arrays.asList(
-            makeElementOfKind(TypeElement.class, ElementKind.CLASS),
-            makeElementOfKind(TypeElement.class, ElementKind.INTERFACE),
-            makeElementOfKind(TypeElement.class, ElementKind.ENUM),
-            makeElementOfKind(TypeElement.class, ElementKind.ANNOTATION_TYPE),
-            makeElementOfKind(ExecutableElement.class, ElementKind.METHOD),
-            makeElementOfKind(ExecutableElement.class, ElementKind.CONSTRUCTOR)
-        );
-        PackageElement packageElement = mock(PackageElement.class);
-        doReturn(packageElements).when(packageElement).getEnclosedElements();
-        when(elementUtils.getPackageElement(PACKAGE_NAME)).thenReturn(packageElement);
-    }
+    public static class AnnotationProcessorImpl extends AnnotationProcessorBase {
 
-    private void whenPackageElementsProcessed() {
-        collectedClasses = collector.collect(PACKAGE_NAME);
-    }
+        private BasePackageClassesCollector collector;
 
-    private void thenCollectorShouldContainOnlyClass() {
-        assertThat(collectedClasses, hasSize(1));
-        assertEquals(ElementKind.CLASS, CollectionUtils.getFirst(collectedClasses).getKind());
+        @Override
+        protected void afterInit(ProcessingEnvironment processingEnvironment, ProcessorContext processorContext) {
+            super.afterInit(processingEnvironment, processorContext);
+            collector = new BasePackageClassesCollector(processorContext);
+        }
+
+        @Override
+        public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+            assertThat(collector.collect("com.example.primary"),
+                TypeElementsSetMatcher.isSetOfTypeElements("com.example.primary.ClassA", "com.example.primary.ClassB"));
+            return false;
+        }
     }
 }
